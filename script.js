71ac1474a5108380b7beb11db9b38a282324b421
ArchdukeChilly <matt.t.childress@gmail.com>
2022-04-08 16:53:24 -0400

    added event listeners to buttons. early idea for grid generation in javascript

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-let canvas = document.getElementById('canvas');
-let slider = document.getElementById('myRange');
-let output = document.getElementById('sizenum');
-const grids = document.getElementById('grids');
-
-
+// need to connect slider as event listener to grid sizing
+// need to make function for appends for more or less divs
+// need to make event listeners for buttons
+// need connect js and css
+let slider = document.getElementById("myRange");
+let output = document.getElementById("sizeNum");
+let setBtn = document.getElementById("setBtn");
+let rstBtn = document.getElementById("rstBtn");
 output.innerHTML = slider.value;
 
 //Reports back the information from slider as it adjusts
-slider.oninput = function() {
+slider.oninput = function () {
     output.innerHTML = this.value;
+    
 }
 
+setBtn.addEventListener('click', () => makeGrid());
+rstBtn.addEventListener('click', () => resetGrid());
 
+let makeGrid = () => {
+    let grids = document.getElementById("grids");
+    for(i = 0; i <100; i++) {
+        let cells = document.createElement("div");
 
-function makeRows(rows,cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0, c< (rows * cols), c++) {
-        let cell = document.createElement('div');
-        cell.innerText = (c +1);
-        container.appendChild(cell).className = 'grid-item';
-    };
-};
+        grids.appendChild("cells");
 
-makeRows(16,16);
+    }
+}
